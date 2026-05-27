f0e7133e5b200ccba675351e53c167fa136cafe0
SeniorMan98 <zurichlatest@gmail.com>
2026-05-26 18:37:54 -0700

    firstcommit

---
+++ My Portfolio/script.js
@@ -0,0 +1,9 @@
+ScrollReveal().reveal('ul, .sec1, .hero, .image, .item, footer, .ft', {
+    delay: 200,
+    opacity: 0,
+    distance: '50px',
+    origin: 'bottom',
+    duration: 1000,
+    interval: 200,
+    reset: true,
+});
